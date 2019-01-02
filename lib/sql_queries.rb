56bfb50b005f87e7dbb1c71371d7daa5eb16470b
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-01-02 23:49:23 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,7 +3,7 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT * FROM bears ORDER BY name;"
+  "SELECT DISTINCT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT DISTINCT name, age FROM bears WHERE age LIKE 20;"
+  "SELECT DISTINCT name, age FROM bears WHERE id LIKE 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
